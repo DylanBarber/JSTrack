17b4a3081b1c23e7bf26c5cd4cf030f28e15899b
Dylan Barber <dbarber@Administrators-MacBook-Pro.local>
2019-06-24 10:27:01 -0400

    Added control flow to disallow end user from breaking the program by entering incorrect number ranges or incorrect end of iteration number. Also added GUI to display information about the program and inputs

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,19 @@
 var lowNumber = parseInt(prompt('Insert lowest number'));
 var highNumber = parseInt(prompt('Insert highest number'));
 var endIter = parseInt(prompt('Insert when to end iteration'));
-console.log('End iteration at: ' + endIter);
-var i;
-var counter = 0; 
-while (i != endIter) {
+if (endIter <= highNumber && endIter >= lowNumber && lowNumber < highNumber) {
+    document.write('<p>Console will end iteration at: ' + endIter + '</p><br>');
+    var i;
+    var counter = 0; 
+    while (i != endIter) {
     counter += 1
     var i = Math.floor(Math.random() * (highNumber - lowNumber + 1) + lowNumber);
     console.log(i);
     if (i === endIter) {
-        console.log('End of iterations reached at iteration: ' + counter)
+        document.write('<p>Desired number reached at iteration: ' + counter + '</p><br>')
+        document.write('<b><p>You can check the iterations that were ran in the console</p></b>')
     }
-};
+    };
+} else {
+    console.log('Your selected end of iteration number is not within range or your selected range is incorrect')
+}
