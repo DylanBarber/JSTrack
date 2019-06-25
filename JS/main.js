bb223ac45c3f25a03c3a9f32293945b3cb55269a
Dylan Barber <DylanBarber1@Outlook.com>
2019-06-25 12:49:22 -0400

    reverted

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,14 @@
+var lowNumber = parseInt(prompt('Insert lowest number'));
+var highNumber = parseInt(prompt('Insert highest number'));
+var endIter = parseInt(prompt('Insert when to end iteration'));
+console.log('End iteration at: ' + endIter);
 var i;
-for (i = 0; i > 5; i++) { 
-  console.log(i);
-}
+var counter = 0; 
+while (i != endIter) {
+    counter += 1
+    var i = Math.floor(Math.random() * (highNumber - lowNumber + 1) + lowNumber);
+    console.log(i);
+    if (i === endIter) {
+        console.log('End of iterations reached at iteration: ' + counter)
+    }
+};
