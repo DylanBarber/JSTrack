4eeed4aa6854bf2184d9a5aefbbce7aebdc397ef
Dylan Barber <DylanBarber1@Outlook.com>
2019-06-25 12:48:07 -0400

    Commit before revert

--- JS/main.js
+++ JS/main.js
@@ -1,20 +1,4 @@
-var lowNumber = parseInt(prompt('Insert lowest number'));
-var highNumber = parseInt(prompt('Insert highest number'));
-var endIter = parseInt(prompt('Insert when to end iteration'));
-if (endIter <= highNumber && endIter >= lowNumber && lowNumber < highNumber) {
-    document.write('<p>Console will end iteration at: ' + endIter + '</p><br>');
-    var i;
-    var counter = 0; 
-    while (i != endIter) {
-    counter += 1
-    var i = Math.floor(Math.random() * (highNumber - lowNumber + 1) + lowNumber);
-    console.log(i);
-    if (i === endIter) {
-        document.write('<p>Desired number reached at iteration: ' + counter + '</p><br>')
-        document.write('<b><p>You can check the iterations that were ran in the console</p></b><br>')
-        document.write('<b><p>Reload your webpage to run the program again</p></b>')
-    }
-    };
-} else {
-    console.log('Your selected end of iteration number is not within range or your selected range is incorrect')
+var i;
+for (i = 0; i > 5; i++) { 
+  console.log(i);
 }
